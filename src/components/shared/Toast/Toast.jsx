9555677ccb73960a962b93d8223c4e6ed84da809
César Córdoba <cordobacesar29@gmail.com>
2021-12-19 16:14:51 -0300

    Toast component

---
+++ src/components/shared/Toast/Toast.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+import { Tag } from '@dexma/ui-components';
+import './Toast.css';
+
+export const Toast = ({label}) => {
+  return ( 
+    <div className="toast">
+      <Tag color="gray" variant="outline">{label}</Tag>
+    </div>
+  )
+};
