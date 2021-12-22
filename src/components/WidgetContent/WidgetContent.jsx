1b6fce81e3edc26f4e0bc0deb6a52ab0da7de961
gonzalo gilq <gonzalogg1975@gmail.com>
2021-12-22 10:39:25 -0300

    fuente central, agregar 2 filas,compo-superior

--- src/components/WidgetContent/WidgetContent.jsx
+++ src/components/WidgetContent/WidgetContent.jsx
@@ -1,39 +1,53 @@
 import React from 'react'
 import { Dot, ToastContainer, WidgetLabels } from '../shared';
-import {Widget} from '../'
+import { Widget } from '../'
 
 
 const widgetData = [
-    {
-      number: "126",
-      string: "1dashboard",
-      icon: <Dot style={{ backgroundColor: "red" }} />,
-      clase: 'icon1',
+  {
+    number: "126",
+    string: "1dashboard",
 
-    },
-    {
-      number: "456",
-      string: "1dashboard",
-      icon: <Dot style={{ backgroundColor: "red" }} />,
-      clase: 'icon1',
-      toast: true
+    clase: 'icon1',
 
-    },
-    { number: "125", clase: 'icon1', string: 'dashboard', icon: <Dot style={{ backgroundColor: "red" }} /> },
-    { number: "16", string: "1dashboard", clase: 'icon1' },
-    { number: "89%", clase: 'icon1' },
-  ];
+  },
+  {
+    number: "456",
+    string: "1dashboard",
+    icon: <Dot style={{ backgroundColor: "red" }} />,
+    clase: 'icon1',
+    toast: true
 
-  const widgets = widgetData.map((w) => (
-    <Widget key={w.number} >
-      {w.toast ? <ToastContainer label='prueba' /> : <WidgetLabels {...w} />}
-    </Widget>
+  },
+  {
+    number: "16",
+    clase: 'icon1',
+    string: 'dashboard',
+    icon: <Dot style={{ backgroundColor: "red" }} />
+  },
 
-  ));
+  {
+    number: "21",
+    string: "1dashboard",
+    clase: 'icon1',
+    icon: <Dot style={{ backgroundColor: "red" }} />,
+  },
+  {
+    number: "5%",
+    clase: 'icon1'
+  },
+];
+
+const widgets = widgetData.map((w) => (
+  <Widget key={w.number} >
+    {w.toast ? <ToastContainer label='prueba' /> : <WidgetLabels {...w} />}
+  </Widget>
+
+));
 export const WidgetContent = () => {
-    return (
-        <>
-            {widgets}
-        </>
-    )
+  return (
+    <>
+      {widgets}
+    </>
+  )
 }
