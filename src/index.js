66fc5babdde7cc5985c1b1917b0eaa3bffe3da62
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-01-03 21:37:16 -0600

    Testing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './fonts/SFProText-Regular.ttf';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./fonts/SFProText-Regular.ttf";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 
-
-  document.getElementById('root')
+  document.getElementById("root")
 );
