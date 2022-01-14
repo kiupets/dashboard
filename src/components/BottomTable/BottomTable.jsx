cc621f28ba78a120fa0922e91f8a83ed39cab83c
César Córdoba <cordobacesar29@gmail.com>
2022-01-14 11:29:24 -0300

    add loading component

--- src/components/BottomTable/BottomTable.jsx
+++ src/components/BottomTable/BottomTable.jsx
@@ -1,4 +1,5 @@
 import React,{useEffect, useState} from "react";
+import { Dots } from "@dexma/ui-components";
 import { IncidentsRow } from "../shared/IncidentsRow/IncidentsRow";
 
 import { info3 } from "../../data";
@@ -31,14 +32,14 @@ export const BottomTable = () => {
     <>
       <IncidentsRow 
         className="total-incidencias"
-        comunication={data.Comunicacion ? data.Comunicacion : "no data"}
-        pasarelaClima={data.PasarelaClima ? data.PasarelaClima : "no data"}
-        alumbrado={data.Alumbrado ? data.Alumbrado : "no data"}
-        clima={data.Clima ? data.Clima : "no data"}
-        banderola={data.Banderola ? data.Banderola : "no data"}
-        rotulo={data.Rotulos ? data.Rotulos : "no data"}
-        consumoClima={data.ConsumoClima ? data.ConsumoClima : "no data"}
-        confort={data.Confort ? data.Confort : "no data"}
+        comunication={data.Comunicacion ? data.Comunicacion : <Dots steps={3} size={2} />}
+        pasarelaClima={data.PasarelaClima ? data.PasarelaClima : <Dots steps={3} size={2} />}
+        alumbrado={data.Alumbrado ? data.Alumbrado : <Dots steps={3} size={2} />}
+        clima={data.Clima ? data.Clima : <Dots steps={3} size={2} />}
+        banderola={data.Banderola ? data.Banderola : <Dots steps={3} size={2} />}
+        rotulo={data.Rotulos ? data.Rotulos : <Dots steps={3} size={2} />}
+        consumoClima={data.ConsumoClima ? data.ConsumoClima : <Dots steps={3} size={2} />}
+        confort={data.Confort ? data.Confort : <Dots steps={3} size={2} />}
       />
     </>
   )
