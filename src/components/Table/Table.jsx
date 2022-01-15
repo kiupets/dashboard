7bdebb02250df6891d99ee00b5b709f6ed3e959e
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-01-15 11:41:58 -0600

    Fix some bugs add api call

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,36 +1,44 @@
-import React from "react";
-import { index, info } from "../../data";
+import React, { useState } from "react";
 import { Span, TableRow, Dot, TableData, TableHeader } from "../shared/";
 import { BottomTable } from "../BottomTable/BottomTable";
+import * as R from "ramda";
 import "./Table.css";
 
-const keys = data.map(item => Object.keys(item)).map(item => item[0])
-const items = data.map((item, i) => item[keys[i]])
-const headers = Object.keys(items[0]).map(header => header.replace('_', ' '))
+export const Table = ({data}) => {
+  const [keys, setKeys] = useState([]);
+  const [items, setItems] = useState([]);
+  const [headers, setHeaders] = useState([]);
 
 
-export const Table = () => {
-  const [dataSort, setDataSort] = useState(items)
+if(data.length !==0){
+
+  setKeys(data?.map((item) => Object?.keys(item))?.map((item) => item[0]));
+  setItems(data?.map((item, i) => item[keys[i]]));
+  setHeaders(Object?.keys(items[0]).map((header) =>
+    header?.replace("_", " ")
+  ));
+}
+ 
+  const [dataSort, setDataSort] = useState(items);
 
   const orderData = (e) => {
-    const key = e.target.innerText
-    key === 'Ciudad' || key === 'Tipologia'
+    const key = e.target.innerText;
+    key === "Ciudad" || key === "Tipologia"
       ? setDataSort(R.sort(R.ascend(R.prop(key)), dataSort))
-      : setDataSort(R.sort(R.descend(R.prop(key.replace(' ', '_'))), dataSort))
-
-  }
+      : setDataSort(R.sort(R.descend(R.prop(key.replace(" ", "_"))), dataSort));
+  };
 
   return (
-    <div style={{ padding: '30px' }}>
+    <div style={{ padding: "30px" }}>
       <table className="table">
         <thead>
           <TableRow style={{ width: "100%" }}>
-            {headers.map((header, i) => (
+            {headers?.map((header, i) => (
               <TableHeader key={i}>
                 <Span
                   style={{ cursor: "pointer" }}
                   onClick={orderData}
-                  className={`span-header ${header === 'ID' ? "index" : ""}`}
+                  className={`span-header ${header === "ID" ? "index" : ""}`}
                   label={header}
                 />
               </TableHeader>
@@ -43,8 +51,12 @@ export const Table = () => {
               <TableData className="table-index">
                 <Span
                   className="span-index"
-                  style={{ textDecoration: "underline", cursor: "pointer", width: '90px' }}
-                  label={item['ID']}
+                  style={{
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                    width: "90px",
+                  }}
+                  label={item["ID"]}
                 />
               </TableData>
 
@@ -57,72 +69,123 @@ export const Table = () => {
               </TableData>
 
               <TableData>
-                {item.Comunicación !== '-' ? (
-                  <Dot style={{ backgroundColor: `${item.Comunicación ? '#00CC87' : '#F25A5A'}` }} />
+                {item.Comunicación !== "-" ? (
+                  <Dot
+                    style={{
+                      backgroundColor: `${
+                        item.Comunicación ? "#00CC87" : "#F25A5A"
+                      }`,
+                    }}
+                  />
                 ) : (
                   <Span className="span-no-data" label="-" />
                 )}
               </TableData>
               <TableData>
-                {item.Tipologia !== 'Completa' ? (
+                {item.Tipologia !== "Completa" ? (
                   <Span className="span-no-data" label="-" />
                 ) : (
-                  <Dot style={{ backgroundColor: `${item.Pasarela_Clima ? '#00CC87' : '#F25A5A'}` }} />
+                  <Dot
+                    style={{
+                      backgroundColor: `${
+                        item.Pasarela_Clima ? "#00CC87" : "#F25A5A"
+                      }`,
+                    }}
+                  />
                 )}
               </TableData>
               <TableData>
-                {item['Alumbrado'] !== '-' ? (
-                  <Dot style={{ backgroundColor: `${item.Alumbrado ? '#00CC87' : '#F25A5A'}` }} />
-                ) : (
-                  <Span className="span-no-data" label="-" />
-                )}
-              </TableData>
-              <TableData>
-                {item['Clima'] !== '-' ? (
-                  <Dot style={{ backgroundColor: `${item.Clima ? '#00CC87' : '#F25A5A'}` }} />
-                ) : (
-                  <Span className="span-no-data" label="-" />
-                )}
-              </TableData>
-              <TableData>
-                {item.Banderola !== '-' ? (
-                  <Dot style={{ backgroundColor: `${item.Banderola ? '#00CC87' : '#F25A5A'}` }} />
+                {item["Alumbrado"] !== "-" ? (
+                  <Dot
+                    style={{
+                      backgroundColor: `${
+                        item.Alumbrado ? "#00CC87" : "#F25A5A"
+                      }`,
+                    }}
+                  />
                 ) : (
                   <Span className="span-no-data" label="-" />
                 )}
               </TableData>
               <TableData>
-                {item.Rotulos !== '-' ? (
-                  <Dot style={{ backgroundColor: `${item.Rotulos ? '#00CC87' : '#F25A5A'}` }} />
+                {item["Clima"] !== "-" ? (
+                  <Dot
+                    style={{
+                      backgroundColor: `${item.Clima ? "#00CC87" : "#F25A5A"}`,
+                    }}
+                  />
                 ) : (
                   <Span className="span-no-data" label="-" />
                 )}
               </TableData>
               <TableData>
-                {item.Consumo_Clima !== '-' ? (
-                  <Dot style={{ backgroundColor: `${item.Consumo_Clima ? '#00CC87' : '#F25A5A'}` }} />
+                {item.Banderola !== "-" ? (
+                  <Dot
+                    style={{
+                      backgroundColor: `${
+                        item.Banderola ? "#00CC87" : "#F25A5A"
+                      }`,
+                    }}
+                  />
+                ) : (
+                  <Span className="span-no-data" label="-" />
+                )}
+              </TableData>
+              <TableData>
+                {item.Rotulos !== "-" ? (
+                  <Dot
+                    style={{
+                      backgroundColor: `${
+                        item.Rotulos ? "#00CC87" : "#F25A5A"
+                      }`,
+                    }}
+                  />
+                ) : (
+                  <Span className="span-no-data" label="-" />
+                )}
+              </TableData>
+              <TableData>
+                {item.Consumo_Clima !== "-" ? (
+                  <Dot
+                    style={{
+                      backgroundColor: `${
+                        item.Consumo_Clima ? "#00CC87" : "#F25A5A"
+                      }`,
+                    }}
+                  />
                 ) : (
                   <Span className="span-no-data" label={item.Consumo_Clima} />
                 )}
               </TableData>
               <TableData>
-                {item['Confort'] !== '-' ? (
-                  <Dot style={{ backgroundColor: `${item.Confort ? '#00CC87' : '#F25A5A'}` }} />
+                {item["Confort"] !== "-" ? (
+                  <Dot
+                    style={{
+                      backgroundColor: `${
+                        item.Confort ? "#00CC87" : "#F25A5A"
+                      }`,
+                    }}
+                  />
                 ) : (
                   <Span className="span-no-data" label={item.Confort} />
                 )}
               </TableData>
               <TableData>
-                {item['Anomalías'] !== '-' ?
+                {item["Anomalías"] !== "-" ? (
                   <Span className="span-info" label={item.Anomalías} />
-                  : <Span className="span-no-data" label={item.Anomalías} />
-                }
+                ) : (
+                  <Span className="span-no-data" label={item.Anomalías} />
+                )}
               </TableData>
               <TableData>
-                {item.Impacto_Anomalías !== '-' ?
+                {item.Impacto_Anomalías !== "-" ? (
                   <Span className="span-info" label={item.Impacto_Anomalías} />
-                  : <Span className="span-no-data" label={item.Impacto_Anomalías} />
-                }
+                ) : (
+                  <Span
+                    className="span-no-data"
+                    label={item.Impacto_Anomalías}
+                  />
+                )}
               </TableData>
               <TableData>
                 <Span className="span-info" label={`${item.id}€`} />
@@ -133,6 +196,5 @@ export const Table = () => {
         </tbody>
       </table>
     </div>
-
   );
 };
---
+++ src/utils/sortInfo.js
@@ -0,0 +1,26 @@
+export const sortInfo = (array1, array2) => {
+  let falseCount, trueCount;
+  const sortData = {};
+  let percent = 0;
+  array1.map((incident) => {
+    falseCount = 0;
+    trueCount = 0;
+    array2.map((info) => {
+      if (info[incident] === true) {
+        trueCount += 1;
+      } else if (info[incident] === false) {
+        falseCount += 1;
+      }
+    });
+    percent =
+      falseCount === 0 && trueCount === 0
+        ? 0
+        : (falseCount / (falseCount + trueCount)).toFixed(1);
+    sortData[incident] = {
+      false: falseCount,
+      true: trueCount,
+      percent: percent,
+    };
+  });
+  return sortData;
+};
