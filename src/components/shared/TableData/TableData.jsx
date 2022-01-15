7c7e01f37703d4bace2e1654d233ce31ee0e4c1e
César Córdoba <cordobacesar29@gmail.com>
2022-01-14 21:16:24 -0300

    agregandoo estilos mediante props

--- src/components/shared/TableData/TableData.jsx
+++ src/components/shared/TableData/TableData.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./TableData.css";
 
-export const TableData = ({ children, className, colspan }) => {
-  return <td style={{ width: '108px' }} colSpan={colspan} className={`table-data ${className}`}>{children}</td>;
+export const TableData = ({ children, className, colspan,...props }) => {
+  return <td  {...props} colSpan={colspan} className={`table-data ${className}`}>{children}</td>;
 };
 
 //  default TableData;
