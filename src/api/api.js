802b234de1f91494044997c9558c938dff8400fb
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-01-21 10:16:34 -0600

    Add api calls

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 export const baseUrl = process.env.REACT_APP_BACKEND_URL;
 const urlParams = new URLSearchParams(window.location.search);
-// const dep_id = urlParams.get("dep_id");
-// const session_id = urlParams.get("session_id");
+const dep_id = urlParams.get("dep_id");
+const session_id = urlParams.get("session_id");
 const headers = {
-  "X-Dep-ID": '5679',
-  "X-Session-ID": '671de1b2bf9f9ffb62262f034fc0db277e12eab99228a5d2b4135aad3b00f407',
+  "X-Dep-ID": dep_id,
+  "X-Session-ID": session_id,
 };
 const get = async (path) => {
   const response = await axios.get(`${baseUrl}${path}/`, { headers });
@@ -15,5 +15,5 @@ const get = async (path) => {
 export const api = {
   getLocationTags: () => get("locations_tags"),
   getLocations: () => get("locations"),
-  getDataTable: () => get("table")
+  getDataTable: () => get("table"),
 };
