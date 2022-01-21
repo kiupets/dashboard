060f8792318b82e66638e009d82f078afe0490bb
gonzalo gil <gonzalogg1975@gmail.com>
2022-01-21 00:34:40 -0300

    headers alignment

--- src/components/Headers/Headers.jsx
+++ src/components/Headers/Headers.jsx
@@ -4,24 +4,29 @@ import { headersData } from "./headersData";
 import "./headers.css";
 export const Headers = ({ orderData }) => {
   return (
-      <table className="table">
-        <thead>
-          <TableRow style={{ width: "100%" }}>
-            {headersData?.map((header, i) => (
-              <TableHeader className={`${header === "ID" ? "index" : ""}`} key={i}>
-                <Span
-                  style={{
-                    cursor: "pointer",
-                    paddingLeft: "18px",
-                  }}
-                  onClick={orderData}
-                  className={`${header === "ID" ? "span-special" : "span-header"}`}
-                  label={header}
-                />
-              </TableHeader>
-            ))}
-          </TableRow>
-        </thead>
-      </table>
+    <table className="table">
+      <thead>
+        <TableRow style={{ width: "100%" }}>
+          {headersData?.map((header, i) => (
+            <TableHeader className={`${header === "ID" ? "index" : ""}`} key={i}>
+              <Span
+                style={{
+                  cursor: "pointer",
+                  paddingLeft: "5px",
+                }}
+                onClick={orderData}
+                className={`${header === "ID" ?
+                  "span-special" :
+                  header === 'Impacto Anomalías'
+                    || header === 'Ahorro Potencial'
+                    ? "span-header-anomalia"
+                    : "span-header"}`}
+                label={header}
+              />
+            </TableHeader>
+          ))}
+        </TableRow>
+      </thead>
+    </table>
   );
 };
