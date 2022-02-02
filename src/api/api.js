cafb5906b922b75d77dffe1323982418b40bb986
Carlos Eduardo Damián Perez <dapecarlos@gmail.com>
2022-02-02 11:58:14 -0300

    Update 'src/api/api.js'

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 export const baseUrl = process.env.REACT_APP_BACKEND_URL;
 const urlParams = new URLSearchParams(window.location.search);
-// const dep_id = urlParams.get("dep_id");
-// const session_id = urlParams.get("session_id");
+const dep_id = urlParams.get("dep_id");
+const session_id = urlParams.get("session_id");
 const headers = {
-  "X-Dep-ID": 5679,
-  "X-Session-ID": "be57ac35e9760ef40a9075a2532e325613f8182e1e0dd6badbdd0c08f2044623",
+  "X-Dep-ID": dep_id,
+  "X-Session-ID": session_id,
 };
 const get = async (path) => {
   const response = await axios.get(`${baseUrl}${path}/`, { headers });
