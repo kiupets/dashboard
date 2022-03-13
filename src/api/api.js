3b896a9cadfb186b74f4911923b3dea1a6cde287
carlos eduardo damian perez <dapecarlos@gmail.com>
2022-03-13 17:54:06 -0600

    Version bump

--- src/api/api.js
+++ src/api/api.js
@@ -4,9 +4,10 @@ const urlParams = new URLSearchParams(window.location.search);
 const dep_id = urlParams.get('dep_id');
 const session_id = urlParams.get('session_id');
 const headers = {
-	'X-Dep-Id': 'dep_id',
-	'X-Session-Id': 'session_id'
+	'X-Dep-Id': dep_id,
+	'X-Session-Id': session_id
 };
+
 
 const get = async (path) => {
 	const response = await axios.get(`${baseUrl}${path}/`, { headers });
